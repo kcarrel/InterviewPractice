62e11225861e0a32d2ce60ba1b1cffe87f1dfdff
Katie Carrel <katiecarrel@comcast.net>
2019-08-17 13:00:56 -0700

    longest common prefix

--- arrays_strings.rb
+++ arrays_strings.rb
@@ -260,3 +260,19 @@ def max_area(height)
     end
     water
 end
+
+def longest_common_prefix(strs)
+    return "" if strs == []
+    common_pre = ""
+    short_str = (strs.sort!).delete_at(0)
+    short_str.each_char do |c|
+         strs.each do |str|
+            if(str.start_with?(common_pre + c))
+                next
+            else
+                return common_pre
+            end
+         end
+        common_pre += c
+    end
+end
