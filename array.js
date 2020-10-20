cdcd41d2a910760ce965c9a0b830050fbef8e97a
Katie Carrel <katiecarrel@comcast.net>
2020-10-20 14:19:43 -0700

    twosum js

--- array.js
+++ array.js
@@ -9,3 +9,17 @@ var removeDuplicates = function(nums) {
     return pointer;
 };
 
+
+var twoSum = function(nums, target) {
+    var Hash = {};
+    var result = [];
+    for(var i = 0; i < nums.length; i++) {
+        var difference = target - nums[i]
+        var index = Hash[difference]
+        if (typeof index !== 'undefined') {
+            return [index, i];
+        } else {
+            Hash[nums[i]] = i; 
+        }
+    }
+};
